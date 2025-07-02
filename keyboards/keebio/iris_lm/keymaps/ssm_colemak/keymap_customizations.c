48d09f8a71560e8b63e9b9a8874bd9cdfcba7c42
Spencer McOmber <spencer.mcomber@gmail.com>
2025-07-01 23:48:58 -0600

    Enable space on tap, cmd on hold for space key

--- keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap_customizations.c
+++ keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap_customizations.c
@@ -37,7 +37,9 @@ bool rgb_matrix_indicators_user(void) {
 // https://docs.qmk.fm/features/tap_dance#example-3
 enum {
     // Enum for the "enter" key tap dance
-    CT_ENT
+    CT_ENT,
+    // Enum for the "space" key tap dance
+    CT_SPC
 };
 
 typedef struct {
@@ -81,6 +83,7 @@ void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
     [CT_ENT] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, KC_LOPT),
+    [CT_SPC] = ACTION_TAP_DANCE_TAP_HOLD(KC_SPC, KC_RCMD),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -105,6 +108,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // Handle tap dance
     switch (keycode) {
         case TD(CT_ENT): // list all tap dance keycodes with tap-hold configurations
+        case TD(CT_SPC):
             if (!record->event.pressed) {
                 tap_dance_action_t *action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
                 if (action->state.count && !action->state.finished) {
