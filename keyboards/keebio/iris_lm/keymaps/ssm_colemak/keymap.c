ed38a340fcacbb20fbc875dfa9d39fcf9f89a984
Spencer McOmber <spencer.mcomber@gmail.com>
2025-07-01 23:29:40 -0600

    Enable tap dance for enter key

--- keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap.c
@@ -3,6 +3,12 @@
 #    include "keymap.h"
 #endif
 
+// Tap dance configuration to make the "enter" key work as an "opt" / "alt" key
+// https://docs.qmk.fm/features/tap_dance#example-3
+enum {
+    // Enum for the "enter" key tap dance
+    CT_ENT
+};
 
 /* THIS FILE WAS GENERATED!
  *
@@ -11,31 +17,10 @@
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS, KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TL_LOWR, TL_UPPR, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, KC_ENT, TG(3), KC_BSPC, KC_SPC, KC_RALT),
+    [0] = LAYOUT(QK_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS, KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TL_LOWR, TL_UPPR, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, TD(CT_ENT), TG(3), KC_BSPC, KC_SPC, KC_RALT),
     [1] = LAYOUT(KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP, KC_GRV, QK_BOOT, KC_TRNS, KC_UP, KC_TRNS, KC_LBRC, KC_RBRC, KC_P7, KC_P8, KC_P9, KC_P0, KC_PGDN, KC_DEL, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR, KC_RCBR, KC_P4, KC_P5, KC_P6, KC_PLUS, KC_PIPE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LPRN, KC_TRNS, KC_TRNS, KC_RPRN, KC_P1, KC_P2, KC_P3, KC_MINS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_DEL, KC_TRNS, KC_P0),
     [2] = LAYOUT(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, RM_TOGG, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, QK_BOOT, RM_VALU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_MACRO_0, QK_MACRO_1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, RM_VALD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_MACRO_2, KC_TRNS, KC_TRNS, QK_MACRO_3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EE_CLR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-    [3] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TL_LOWR, TL_UPPR, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, KC_ENT, KC_TRNS, KC_BSPC, KC_SPC, KC_RALT)
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch (keycode) {
-            case QK_MACRO_0:
-                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_LBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
-                return false;
-            case QK_MACRO_1:
-                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_RBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
-                return false;
-            case QK_MACRO_2:
-                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_LEFT)SS_UP(X_LCMD)SS_UP(X_LCTL));
-                return false;
-            case QK_MACRO_3:
-                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_RGHT)SS_UP(X_LCMD)SS_UP(X_LCTL));
-                return false;
-        }
-    }
-
-    return true;
+    [3] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, TL_LOWR, TL_UPPR, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, TD(CT_ENT), KC_TRNS, KC_BSPC, KC_SPC, KC_RALT)
 };
 
 // Disable backlight animations, just use solid color
@@ -69,6 +54,86 @@ bool rgb_matrix_indicators_user(void) {
     }
     return false;
 }
+
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                                        \
+    {                                                                               \
+        .fn        = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
+        .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
+    }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [CT_ENT] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, KC_LOPT),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Handle macros
+    if (record->event.pressed) {
+        switch (keycode) {
+            case QK_MACRO_0:
+                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_LBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
+                return false;
+            case QK_MACRO_1:
+                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_RBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
+                return false;
+            case QK_MACRO_2:
+                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_LEFT)SS_UP(X_LCMD)SS_UP(X_LCTL));
+                return false;
+            case QK_MACRO_3:
+                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_RGHT)SS_UP(X_LCMD)SS_UP(X_LCTL));
+                return false;
+        }
+    }
+
+    // Handle tap dance
+    switch (keycode) {
+        case TD(CT_ENT): // list all tap dance keycodes with tap-hold configurations
+            if (!record->event.pressed) {
+                tap_dance_action_t *action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+                if (action->state.count && !action->state.finished) {
+                    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                    tap_code16(tap_hold->tap);
+                }
+            }
+            break;
+    }
+
+    return true;
+};
+
+// End tap dance configuration
 
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
--- keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap_customizations.c
+++ keyboards/keebio/iris_lm/keymaps/ssm_colemak/keymap_customizations.c
@@ -32,3 +32,90 @@ bool rgb_matrix_indicators_user(void) {
     }
     return false;
 }
+
+// Tap dance configuration to make the "enter" key work as an "opt" / "alt" key
+// https://docs.qmk.fm/features/tap_dance#example-3
+enum {
+    // Enum for the "enter" key tap dance
+    CT_ENT
+};
+
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold)                                        \
+    {                                                                               \
+        .fn        = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, \
+        .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}),               \
+    }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [CT_ENT] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, KC_LOPT),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Handle macros
+    if (record->event.pressed) {
+        switch (keycode) {
+            case QK_MACRO_0:
+                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_LBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
+                return false;
+            case QK_MACRO_1:
+                SEND_STRING(SS_DOWN(X_LSFT)SS_DOWN(X_LCMD)SS_TAP(X_RBRC)SS_UP(X_LCMD)SS_UP(X_LSFT));
+                return false;
+            case QK_MACRO_2:
+                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_LEFT)SS_UP(X_LCMD)SS_UP(X_LCTL));
+                return false;
+            case QK_MACRO_3:
+                SEND_STRING(SS_DOWN(X_LCTL)SS_DOWN(X_LCMD)SS_TAP(X_RGHT)SS_UP(X_LCMD)SS_UP(X_LCTL));
+                return false;
+        }
+    }
+
+    // Handle tap dance
+    switch (keycode) {
+        case TD(CT_ENT): // list all tap dance keycodes with tap-hold configurations
+            if (!record->event.pressed) {
+                tap_dance_action_t *action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+                if (action->state.count && !action->state.finished) {
+                    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                    tap_code16(tap_hold->tap);
+                }
+            }
+            break;
+    }
+
+    return true;
+};
+
+// End tap dance configuration
